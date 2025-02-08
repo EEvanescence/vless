5b2a22052ae3f7360bfa08fd2ea9e2edc3b0eab3
nesa <127026836+EEvanescence@users.noreply.github.com>
2025-02-08 21:06:37 +0330

    Update spring.py

--- edge/spring.py
+++ edge/spring.py
@@ -80,18 +80,21 @@ def toSingBox(tag, clean_ip, detour):
         try:
             data = json.loads(output)
             wg = {
-                "tag": tag,
                 "type": "wireguard",
+                "tag": tag,
+                "name": "wg0",
+                "system": true,
+                "mtu": 1280,
                 "address": ["172.16.0.2/32", "2606:4700:110:8735:bb29:91bc:1c82:aa73/128"],
                 "private_key": f"{data['private_key']}",
-                "mtu": 1300,
                 "peers": [
                     {
                         "address": f"{clean_ip.split(':')[0]}",
                         "port": int(clean_ip.split(":")[1]),
                         "public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                        "reserved": data["config"]["reserved"],
                         "allowed_ips": ["0.0.0.0/0", "::/0"],
+                        "persistent_keepalive_interval": 30,
+                        "reserved": data["config"]["reserved"],
                     }
                 ],
                 "detour": f"{detour}",
